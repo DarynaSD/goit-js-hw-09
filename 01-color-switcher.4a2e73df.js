6af6cacc56239bc5c21550f569848c44765d0723
DarynaSD <122695859+DarynaSD@users.noreply.github.com>
2023-07-19 14:40:25 +0000

    Deploying to gh-pages from @ DarynaSD/goit-js-hw-09@6ffff473ceac1046171d5034ced6c9acb54f7ca7 🚀

---
+++ 01-color-switcher.4a2e73df.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),r=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),r=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(r)}))}();
+//# sourceMappingURL=01-color-switcher.4a2e73df.js.map
